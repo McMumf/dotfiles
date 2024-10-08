06f8e0b96f9fb464a8d226a4d4f1a4f41e3afda9
Carson <mccaffreycarson@proton.me>
2024-10-07 21:57:26 -0600

    Some basic powershell config

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -5,3 +5,25 @@
 Set-Alias -Name gs -Value 'git status'
 Set-Alias -Name ga -Value 'git add'
 Set-Alias -Name gd -Value 'git diff'
+
+## Commands
+
+Set-Alias -Name us -Value Update-Software
+
+function Update-Software {
+    <#
+    .SYNOPSIS
+        Updates all software installed via Winget & Chocolatey. Alias: us
+    #>
+    Write-Verbose "Updating software installed via Winget & Chocolatey"
+    sudo cache on
+    sudo winget upgrade --all --include-unknown --silent --verbose
+    sudo choco upgrade all -y
+    sudo -k
+    $ENV:SOFTWARE_UPDATE_AVAILABLE = ""
+}
+
+# Env Variables
+$ENV:STARSHIP_CONFIG = "$HOME\starship\starship.toml"
+
+Invoke-Expression (&starship init powershell)
